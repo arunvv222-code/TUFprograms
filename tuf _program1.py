b279a15aaeb188380a52c810af5c984413c6c458
arun <arunvv222@gmail.com>
2026-01-05 10:46:58 +0530

    added qquestion

---
+++ tuf _program1.py
@@ -0,0 +1,12 @@
+
+# complt a funnction print number
+
+class Solution:
+    def printNumber(self):
+        number=int(input("enter a number"))
+
+        print(number)
+
+solution_instance=Solution()
+
+solution_instance.printNumber()
